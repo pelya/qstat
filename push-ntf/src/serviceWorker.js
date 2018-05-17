b004e899a5b13c5ec9d2b288f6c566a48dc05ea9
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-05-17 21:03:07 +0300

    Another attempt at fixing json

--- push-ntf/src/serviceWorker.js
+++ push-ntf/src/serviceWorker.js
@@ -3,31 +3,23 @@ self.addEventListener('push', function (event) {
 		return;
 	}
 
-	const sendNotification = (msg, addr) => {
-		console.log('sendNotification: msg ' + msg);
-		console.log('sendNotification: addr ' + addr);
-		let status = self.registration.showNotification("OpenLieroX", {
+	const sendNotification = (msg) => {
+		return self.registration.showNotification("OpenLieroX", {
 			"body": msg,
 			"icon": "https://liero.1337.cx/openlierox.png",
 			"badge": "https://liero.1337.cx/openlierox-badge.png",
 			"tag": "OpenLieroX",
 		});
-		self.addEventListener('notificationclick', function(event) {
-			event.notification.close();
-			clients.openWindow("openlierox://" + addr);
-		}, false);
-		return status;
 	};
 
 	if (event.data) {
-		const data = JSON.parse(event.data.text());
-		console.log('event.data.text(): ' + event.data.text());
-		console.log('data:');
-		console.log(data);
-		console.log('data.msg:');
-		console.log(data.msg);
-		console.log('data.addr:');
-		console.log(data.addr);
-		event.waitUntil(sendNotification(data.msg, data.addr));
+		event.waitUntil(sendNotification(event.data.json().msg));
 	}
 });
+
+self.addEventListener('notificationclick', function(event) {
+	if (event.data) {
+		clients.openWindow("openlierox://" + event.data.json().addr);
+	}
+	event.notification.close();
+}, false);
