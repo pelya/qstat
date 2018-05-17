d06752cf3a5565b859583ffd43f3ae6db2abfcaa
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-05-17 20:52:23 +0300

    More ntf logs

--- push-ntf/src/serviceWorker.js
+++ push-ntf/src/serviceWorker.js
@@ -3,22 +3,31 @@ self.addEventListener('push', function (event) {
 		return;
 	}
 
-	const sendNotification = (body, address) => {
+	const sendNotification = (msg, addr) => {
+		console.log('sendNotification: msg ' + msg);
+		console.log('sendNotification: addr ' + addr);
 		let status = self.registration.showNotification("OpenLieroX", {
-			body,
+			"body": msg,
 			"icon": "https://liero.1337.cx/openlierox.png",
 			"badge": "https://liero.1337.cx/openlierox-badge.png",
 			"tag": "OpenLieroX",
 		});
 		self.addEventListener('notificationclick', function(event) {
 			event.notification.close();
-			clients.openWindow("openlierox://" + address);
+			clients.openWindow("openlierox://" + addr);
 		}, false);
 		return status;
 	};
 
 	if (event.data) {
-		const message = JSON.parse(event.data.text());
-		event.waitUntil(sendNotification(message.msg, message.addr));
+		const data = JSON.parse(event.data.text());
+		console.log('event.data.text(): ' + event.data.text());
+		console.log('data:');
+		console.log(data);
+		console.log('data.msg:');
+		console.log(data.msg);
+		console.log('data.addr:');
+		console.log(data.addr);
+		event.waitUntil(sendNotification(data.msg, data.addr));
 	}
 });
